292c6bd9ec6bd4f87e1546b0208c168964630840
Kaitlin <klewismurphy@gmail.com>
2021-02-23 19:44:54 -0600

    adds route to display all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,22 @@
 class Api::ProductsController < ApplicationController
+  def all_action
+    @all = Product.all
+
+    @allproducts = []
+    index = 0
+
+    while index < @all.length
+      product = {
+        id: @all[index].id,
+        name: @all[index].name,
+        price: @all[index].price,
+        image_url: @all[index].image_url,
+        description: @all[index].description,
+      }
+      @allproducts << product
+      index += 1
+    end
+
+    render "allproducts.json.jb"
+  end
 end
