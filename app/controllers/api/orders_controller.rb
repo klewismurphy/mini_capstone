4f36ea118bf93bc1056f537747f0f114c1caae63
Kaitlin <klewismurphy@gmail.com>
2021-03-10 19:25:30 -0600

    product ids fixed in index and show jsons. frontend ordering now works

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,7 +2,8 @@ class Api::OrdersController < ApplicationController
   def create
     product = Product.find_by(id: params[:product_id])
     calc_subtotal = product.price * params[:quantity].to_i
-    calc_tax = calc_subtotal * 0.09
+    # calc_tax = calc_subtotal * 0.09 #another way to do it
+    calc_tax = params[:quantity].to_i * product.tax
     calc_total = calc_subtotal + calc_tax
 
     @order = Order.new(
@@ -19,11 +20,13 @@ class Api::OrdersController < ApplicationController
 
   def index
     @orders = current_user.orders
+    # @orders = Order.where(user_id: current_user.id) #another way to do it
     render "index.json.jb"
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
+    # @order = Order.find_by(id: params[:id])
+    @order = current_user.orders.find_by(id: params[:id])
     render "show.json.jb"
   end
 end
